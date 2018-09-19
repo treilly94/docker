270752e8abbc01a79c26c565a00b590c60f40af7
treilly94 <treilly94@live.com>
2018-09-19 15:02:23 +0100

    made infastructure test more simple, fixed kudu issues

--- infastructure_test/spark/data/script.scala
+++ infastructure_test/spark/data/script.scala
@@ -1,3 +1,4 @@
+// https://kudu.apache.org/docs/developing.html
 import org.apache.kudu.spark.kudu._
 import org.apache.kudu.client._
 import collection.JavaConverters._
@@ -5,6 +6,10 @@ import collection.JavaConverters._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 
+// Create kudu context
+val kuduContext = new KuduContext("kudu:7051", spark.sparkContext)
+
+// Create df
 val schema = List(
   StructField("name", StringType, false),
   StructField("age", IntegerType, true)
@@ -20,12 +25,12 @@ val df = spark.createDataFrame(
   StructType(schema)
 )
 
+df.show()
 
-val kuduContext = new KuduContext("kudu-master:7051", spark.sparkContext)
-
-
+// Create table from df
 kuduContext.createTable(
     "test_table", df.schema, Seq("name"),
     new CreateTableOptions()
         .setNumReplicas(1)
         .addHashPartitions(List("name").asJava, 3))
+
